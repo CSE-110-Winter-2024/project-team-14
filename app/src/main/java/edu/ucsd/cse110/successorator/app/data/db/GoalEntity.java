38ed06c37c7a377d7af5552071a7f227e1282a91
Asi44 <aasiyah.shaikh44@gmail.com>
2024-03-10 17:11:06 -0700

    changed goal class and goal entity to accommodate recurring goals

--- app/src/main/java/edu/ucsd/cse110/successorator/app/data/db/GoalEntity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/app/data/db/GoalEntity.java
@@ -5,6 +5,8 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.time.LocalDateTime;
+
 import edu.ucsd.cse110.successorator.lib.domain.Goal;
 
 @Entity(tableName = "goals")
@@ -23,19 +25,28 @@ public class GoalEntity {
     @ColumnInfo(name = "sort_order")
     public int sortOrder;
 
-    GoalEntity(@NonNull String taskText, boolean completed, int sortOrder) {
+    @ColumnInfo(name = "type_of_goal")
+            public String typeOfGoal;
+
+    @ColumnInfo(name = "nextDate")
+            public String nextDate;
+
+    GoalEntity(@NonNull String taskText, boolean completed, int sortOrder, String type, LocalDateTime nextDate) {
         this.taskText = taskText;
         this.completed = completed;
         this.sortOrder = sortOrder;
+        this.typeOfGoal = type;
+        this.nextDate = nextDate.toString();
     }
 
     public static GoalEntity fromGoal(@NonNull Goal goal) {
-        var card = new GoalEntity(goal.taskText(), goal.completed(), goal.sortOrder());
+        var card = new GoalEntity(goal.taskText(), goal.completed(), goal.sortOrder(),
+                goal.type(), goal.nextDate());
         card.id = goal.id();
         return card;
     }
 
     public @NonNull Goal toGoal() {
-        return new Goal(id, taskText, completed, sortOrder);
+        return new Goal(id, taskText, completed, sortOrder, typeOfGoal, LocalDateTime.parse(nextDate));
     }
 }
--- lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Goal.java
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Goal.java
@@ -4,22 +4,45 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.io.Serializable;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Goal implements Serializable {
     private final @Nullable Integer id;
 
     private final @NonNull String taskText;
+
     private final boolean completed;
 
     private final @NonNull Integer sortOrder;
 
+    private LocalDateTime nextDate;
 
-    public Goal(@Nullable Integer id, @NonNull String taskText, boolean completed, int sortOrder) {
+    private String typeOfGoal;
+
+
+    //Constructor which automatically finds the next date to recur on
+    public Goal(@Nullable Integer id, @NonNull String taskText, boolean completed,
+                int sortOrder, String typeOfGoal) {
         this.id = id;
         this.taskText = taskText;
         this.completed = completed;
         this.sortOrder = sortOrder;
+        this.nextDate = this.setNextDate();
+        this.typeOfGoal = typeOfGoal;
+    }
+
+    //Constructor to make a goal from a Database record
+    //and set the date to start recurring on
+    public Goal(@Nullable Integer id, @NonNull String taskText, boolean completed,
+                int sortOrder, String typeOfGoal, LocalDateTime nextDate) {
+        this.id = id;
+        this.taskText = taskText;
+        this.completed = completed;
+        this.sortOrder = sortOrder;
+        this.nextDate = nextDate;
+        this.typeOfGoal = typeOfGoal;
     }
 
     public @Nullable Integer id() {
@@ -34,17 +57,111 @@ public class Goal implements Serializable {
     public int sortOrder() {
         return sortOrder;
     }
+    public String type() {
+        return typeOfGoal;
+    }
+    public LocalDateTime nextDate() {
+        return this.nextDate;
+    }
 
     public Goal withId(int id) {
-        return new Goal(id, this.taskText, this.completed, this.sortOrder);
+        return new Goal(id, this.taskText, this.completed, this.sortOrder, this.typeOfGoal);
     }
 
     public Goal withSortOrder(int sortOrder) {
-        return new Goal(this.id, this.taskText, this.completed, sortOrder);
+        return new Goal(this.id, this.taskText, this.completed, sortOrder, this.typeOfGoal);
     }
 
     public Goal toggleCompleted() {
-        return new Goal(this.id, this.taskText, !this.completed, this.sortOrder);
+        return new Goal(this.id, this.taskText, !this.completed, this.sortOrder, this.typeOfGoal);
+    }
+
+
+
+    public LocalDateTime setNextDate() {
+        var startDate =  LocalDateTime.now();
+        switch(typeOfGoal) {
+
+            case "weekly":
+                this.nextDate = LocalDateTime.now().plusDays(7);
+                break;
+
+            case "monthly":
+                int dayOfMonth = findDayNumberInMonth();
+                DayOfWeek dayOfWeekToRepeat = startDate.getDayOfWeek();
+                LocalDateTime nextMonth = startDate.withDayOfMonth(1).plusMonths(1);
+                int maxDayOfMonth = getLastDayOfMonth(nextMonth.getMonthValue());
+
+                //iterate through the next month until you get to the specific day of the week
+                //e.g. the second Tuesday of the next month
+                int daysFound = 0;
+                for(int j = 1; j <= maxDayOfMonth; j++) {
+                    var currDate = nextMonth.withDayOfMonth(j);
+                    var currDay = currDate.getDayOfWeek();
+                    if(currDay.equals(dayOfWeekToRepeat)) {
+                        daysFound += 1;
+                    }
+                    if (daysFound == dayOfMonth) {
+                        nextDate = currDate;
+                        return nextDate;
+                    }
+                }
+                //if the day doesn't exist in the next month, rollover
+                nextMonth = nextMonth.plusMonths(1);
+                var currDate = nextMonth;
+                var currDay = nextMonth.getDayOfWeek();
+                while(!currDay.equals(dayOfWeekToRepeat)) {
+                    currDate = currDate.plusDays(1);
+                    currDay = currDate.getDayOfWeek();
+                }
+                nextDate = currDate;
+                return nextDate;
+
+            case "yearly":
+                return LocalDateTime.now().plusYears(1);
+
+            default: return startDate;
+
+        }
+        return startDate;
+    }
+
+    public int getLastDayOfMonth(int month) {
+        if(month == 1|| month == 3|| month == 5|| month == 7|| month == 8|| month ==12) {
+            return 31;
+        }
+
+        else if(month == 4|| month == 6|| month == 9|| month == 11) {
+            return 30;
+        }
+        //february
+        //TODO: Account for leap year
+        else {
+            return 28;
+        }
+
+    }
+
+    //Checks which day of the month the start day is,
+    // e.g. the second Tuesday of the month
+
+    private int findDayNumberInMonth() {
+        var startDate = LocalDateTime.now();
+
+        DayOfWeek dayOfWeekToRepeat = startDate.getDayOfWeek();;
+        LocalDateTime currentDayOfMonth;
+        int count = 0;
+        int maxDayOfMonth = getLastDayOfMonth(startDate.getMonthValue());
+        int i = 1;
+
+        do {
+            currentDayOfMonth = startDate.withDayOfMonth(i);
+            if (currentDayOfMonth.getDayOfWeek().equals(dayOfWeekToRepeat)) {
+                count +=1;
+            }
+            i+=1;
+        } while (!startDate.equals(currentDayOfMonth));
+        return count;
     }
 
     @Override
@@ -59,5 +176,7 @@ public class Goal implements Serializable {
     public int hashCode() {
         return Objects.hash(id, taskText, completed, sortOrder);
     }
+
+
 
 }
