0873b4d71bc7794127bfccaa8d96065c93fa5d60
ednavho <ednaho121@gmail.com>
2024-03-14 15:41:18 -0700

    add tests for moving pending tasks to today/tmr

--- app/src/test/java/edu/ucsd/cse110/successorator/app/MainViewModelTest.java
+++ app/src/test/java/edu/ucsd/cse110/successorator/app/MainViewModelTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import edu.ucsd.cse110.successorator.lib.data.InMemoryDataSource;
 import edu.ucsd.cse110.successorator.lib.domain.Goal;
@@ -16,73 +17,73 @@ import edu.ucsd.cse110.successorator.lib.domain.TimeKeeper;
 
 public class MainViewModelTest {
 
-//    @Test
-//    public void append() {
-//        InMemoryDataSource data = new InMemoryDataSource();
-//        TimeKeeper timeKeeper = new InMemoryTimeKeeper();
-//        GoalRepository repo = new SimpleGoalRepository(data);
-//        var mvm = new MainViewModel(repo, timeKeeper);
-//
-//        Goal g = new Goal(null, "do homework", false, 4, "school");
-//        mvm.append(g);
-//
-//        assertEquals("do homework", mvm.getOrderedGoals().getValue().get(0).taskText());
-//    }
-//
-//    @Test
-//    public void getOrderedGoals() {
-//        InMemoryDataSource data = new InMemoryDataSource();
-//        TimeKeeper timeKeeper = new InMemoryTimeKeeper();
-//        GoalRepository repo = new SimpleGoalRepository(data);
-//        var mvm = new MainViewModel(repo, timeKeeper);
-//
-//        mvm.append(new Goal(null, "do homework", false, 4));
-//        var orderedGoals = mvm.getOrderedGoals();
-//        assertEquals(1, orderedGoals.getValue().size());
-//    }
-//
-//    @Test
-//    public void remove() {
-//        InMemoryDataSource data = new InMemoryDataSource();
-//        TimeKeeper timeKeeper = new InMemoryTimeKeeper();
-//        GoalRepository repo = new SimpleGoalRepository(data);
-//        var mvm = new MainViewModel(repo, timeKeeper);
-//
-//        Goal g1 = new Goal(null, "do homework", false, 0);
-//        mvm.append(g1);
-//        mvm.append(new Goal(null, "wash dishes", false, 1));
-//        mvm.remove(1);
-//        var orderedGoals = mvm.getOrderedGoals();
-//
-//        assertEquals(1, orderedGoals.getValue().size());
-//        assertEquals("wash dishes", mvm.getOrderedGoals().getValue().get(0).taskText());
-//    }
-//
-//    @Test
-//    public void updateGoal() {
-//        InMemoryDataSource data = new InMemoryDataSource();
-//        GoalRepository repo = new SimpleGoalRepository(data);
-//        TimeKeeper timeKeeper = new InMemoryTimeKeeper();
-//        var mvm = new MainViewModel(repo, timeKeeper);
-//
-//        Goal g = new Goal(null, "do homework", false, 1);
-//        mvm.append(g);
-//        System.out.println(data.getGoals().get(0).taskText());
-//        //mvm.updateGoal(g);
-//    }
-//
-//    @Test
-//    public void updateTime() {
-//        InMemoryDataSource data = new InMemoryDataSource();
-//        GoalRepository repo = new SimpleGoalRepository(data);
-//        TimeKeeper timeKeeper = new InMemoryTimeKeeper();
-//        var mvm = new MainViewModel(repo, timeKeeper);
-//
-//        mvm.setCurrentDateTime(LocalDateTime.now());
-//        assertEquals(LocalDateTime.now().getHour(), mvm.getCurrentDateTime().getValue().getHour());
-//        assertEquals(LocalDateTime.now().getMinute(), mvm.getCurrentDateTime().getValue().getMinute());
-//
-//    }
+    @Test
+    public void append() {
+        InMemoryDataSource data = new InMemoryDataSource();
+        TimeKeeper timeKeeper = new InMemoryTimeKeeper();
+        GoalRepository repo = new SimpleGoalRepository(data);
+        var mvm = new MainViewModel(repo, timeKeeper);
+
+        Goal g = new Goal(null, "do homework", false, 4, "school", LocalDateTime.now(), false);
+        mvm.append(g);
+
+        assertEquals("do homework", mvm.getOrderedGoals().getValue().get(0).taskText());
+    }
+
+    @Test
+    public void getOrderedGoals() {
+        InMemoryDataSource data = new InMemoryDataSource();
+        TimeKeeper timeKeeper = new InMemoryTimeKeeper();
+        GoalRepository repo = new SimpleGoalRepository(data);
+        var mvm = new MainViewModel(repo, timeKeeper);
+
+        mvm.append(new Goal(null, "do homework", false, 4,"school", LocalDateTime.now(), false));
+        var orderedGoals = mvm.getOrderedGoals();
+        assertEquals(1, orderedGoals.getValue().size());
+    }
+
+    @Test
+    public void remove() {
+        InMemoryDataSource data = new InMemoryDataSource();
+        TimeKeeper timeKeeper = new InMemoryTimeKeeper();
+        GoalRepository repo = new SimpleGoalRepository(data);
+        var mvm = new MainViewModel(repo, timeKeeper);
+
+        Goal g1 = new Goal(null, "do homework", false, 0, "school", LocalDateTime.now(), false);
+        mvm.append(g1);
+        mvm.append(new Goal(null, "wash dishes", false, 1, "school", LocalDateTime.now(), false));
+        mvm.remove(1);
+        var orderedGoals = mvm.getOrderedGoals();
+
+        assertEquals(1, orderedGoals.getValue().size());
+        assertEquals("wash dishes", mvm.getOrderedGoals().getValue().get(0).taskText());
+    }
+
+    @Test
+    public void updateGoal() {
+        InMemoryDataSource data = new InMemoryDataSource();
+        GoalRepository repo = new SimpleGoalRepository(data);
+        TimeKeeper timeKeeper = new InMemoryTimeKeeper();
+        var mvm = new MainViewModel(repo, timeKeeper);
+
+        Goal g = new Goal(null, "do homework", false, 1, "school", LocalDateTime.now(), false);
+        mvm.append(g);
+        System.out.println(data.getGoals().get(0).taskText());
+        //mvm.updateGoal(g);
+    }
+
+    @Test
+    public void updateTime() {
+        InMemoryDataSource data = new InMemoryDataSource();
+        GoalRepository repo = new SimpleGoalRepository(data);
+        TimeKeeper timeKeeper = new InMemoryTimeKeeper();
+        var mvm = new MainViewModel(repo, timeKeeper);
+
+        mvm.setCurrentDateTime(LocalDateTime.now());
+        assertEquals(LocalDateTime.now().getHour(), mvm.getCurrentDateTime().getValue().getHour());
+        assertEquals(LocalDateTime.now().getMinute(), mvm.getCurrentDateTime().getValue().getMinute());
+
+    }
 //    @Test
 //    public void listPersistence3TaskRollover() {
 //        InMemoryDataSource data = new InMemoryDataSource();
@@ -92,11 +93,11 @@ public class MainViewModelTest {
 //
 //        mainViewModel.setCurrentDateTime(LocalDateTime.now());
 //
-//        Goal g = new Goal(null, "do homework", false, 1);
+//        Goal g = new Goal(null, "do homework", false, 1, "school", LocalDateTime.now(), false);
 //        mainViewModel.append(g);
-//        Goal g2 = new Goal(null, "do homework2", false, 2);
+//        Goal g2 = new Goal(null, "do homework2", false, 2, "school", LocalDateTime.now(), false);
 //        mainViewModel.append(g2);
-//        Goal g3 = new Goal(null, "do homework3", false, 3);
+//        Goal g3 = new Goal(null, "do homework3", false, 3, "school", LocalDateTime.now(), false);
 //        mainViewModel.append(g3);
 //
 //        // Get the current date and time
@@ -125,11 +126,11 @@ public class MainViewModelTest {
 //
 //        mainViewModel.setCurrentDateTime(LocalDateTime.now());
 //
-//        Goal g = new Goal(null, "do homework", false, 1);
+//        Goal g = new Goal(null, "do homework", false, 1, "school", LocalDateTime.now(), false);
 //        mainViewModel.append(g);
-//        Goal g2 = new Goal(null, "do homework2", false, 2);
+//        Goal g2 = new Goal(null, "do homework2", false, 2, "school", LocalDateTime.now(), false);
 //        mainViewModel.append(g2);
-//        Goal g3 = new Goal(null, "do homework3", true, 3);
+//        Goal g3 = new Goal(null, "do homework3", true, 3, "school", LocalDateTime.now(), false);
 //        mainViewModel.append(g3);
 //
 //        List<Goal> orderedGoals = mainViewModel.getOrderedGoals().getValue();
@@ -161,11 +162,11 @@ public class MainViewModelTest {
 //
 //        mainViewModel.setCurrentDateTime(LocalDateTime.now());
 //
-//        Goal g = new Goal(null, "do homework", true, 1);
+//        Goal g = new Goal(null, "do homework", true, 1, "school", LocalDateTime.now(), false);
 //        mainViewModel.append(g);
-//        Goal g2 = new Goal(null, "do homework2", true, 2);
+//        Goal g2 = new Goal(null, "do homework2", true, 2, "school", LocalDateTime.now(), false);
 //        mainViewModel.append(g2);
-//        Goal g3 = new Goal(null, "do homework3", true, 3);
+//        Goal g3 = new Goal(null, "do homework3", true, 3, "school", LocalDateTime.now(), false);
 //        mainViewModel.append(g3);
 //
 //        List<Goal> orderedGoals = mainViewModel.getOrderedGoals().getValue();
@@ -184,4 +185,76 @@ public class MainViewModelTest {
 //        assertEquals(orderedGoals2.size(), 0);
 //
 //    }
+
+    @Test
+    public void movePendingToToday() {
+        InMemoryDataSource data = new InMemoryDataSource();
+        TimeKeeper timeKeeper = new InMemoryTimeKeeper();
+        GoalRepository repo = new SimpleGoalRepository(data);
+        var mvm = new MainViewModel(repo, timeKeeper);
+
+        // add a goal here where pending is true. this goal shouldn't be in the assert list count
+        Goal g0 = new Goal(null, "do homework", false, 0,
+                "school", LocalDateTime.now(), true);
+        mvm.append(g0);
+
+        // move pending task to today
+        Goal g1 = new Goal(null, "do homework", false, 0,
+                "school", LocalDateTime.now(), false);
+        mvm.append(g1);
+
+        // this is a tomorrow goal - should not be included in todayGoals list
+        Goal g2 = new Goal(null, "wash laundry", false, 0,
+                "school", LocalDateTime.now().plusDays(1), false);
+        mvm.append(g2);
+
+        // add another today goal
+        Goal g3 = new Goal(null, "do dishes", false, 0,
+                "school", LocalDateTime.now(), false);
+        mvm.append(g3);
+
+        var orderedGoals = mvm.getOrderedGoals();
+        assertEquals(4, orderedGoals.getValue().size());
+
+        // check if todayGoals list should only have 1 today goal
+        List<Goal> todayGoals = orderedGoals.getValue().stream().filter(goal -> goal.getDateAdded()
+                .toLocalDate().equals(LocalDateTime.now().toLocalDate()) && !goal.isPending()).collect(Collectors.toList());
+        assertEquals(2, todayGoals.size());
+    }
+
+    @Test
+    public void movePendingToTomorrow() {
+        InMemoryDataSource data = new InMemoryDataSource();
+        TimeKeeper timeKeeper = new InMemoryTimeKeeper();
+        GoalRepository repo = new SimpleGoalRepository(data);
+        var mvm = new MainViewModel(repo, timeKeeper);
+
+        // add a goal here where pending is true. this goal shouldn't be in the assert list count
+        Goal g0 = new Goal(null, "do homework", false, 0,
+                "school", LocalDateTime.now(), true);
+        mvm.append(g0);
+
+        // move pending task to tomorrow
+        Goal g1 = new Goal(null, "do homework", false, 0,
+                "school", LocalDateTime.now().plusDays(1), false);
+        mvm.append(g1);
+
+        // this is a today goal - should not be included in tomorrowGoals list
+        Goal g2 = new Goal(null, "wash laundry", false, 0,
+                "school", LocalDateTime.now(), false);
+        mvm.append(g2);
+
+        // add another tomorrow goal
+        Goal g3 = new Goal(null, "do homework", false, 0,
+                "school", LocalDateTime.now().plusDays(1), false);
+        mvm.append(g3);
+
+        var orderedGoals = mvm.getOrderedGoals();
+        assertEquals(4, orderedGoals.getValue().size());
+
+        // check if tomorrowGoals list should only have 1 today goal
+        List<Goal> tomorrowGoals = orderedGoals.getValue().stream().filter(goal -> goal.getDateAdded()
+                .toLocalDate().equals(LocalDateTime.now().plusDays(1).toLocalDate()) && !goal.isPending()).collect(Collectors.toList());
+        assertEquals(2, tomorrowGoals.size());
+    }
 }
